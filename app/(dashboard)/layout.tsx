afbdca3ebe9314f5cc9a19e491d73e38c6e6fc32
Arjun <arjun.ambavane@gmail.com>
2025-04-12 20:15:33 +0530

    Add: prisma models

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,16 @@
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { UserButton } from "@clerk/nextjs";
 import { ReactNode } from "react";
 
 const Layout = ({children}:{children:ReactNode}) => {
     return  <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-        <nav>
+        <nav className="flex items-center justify-between border-b border-border h-[60px] px-4 py-2">
             <Logo />
+            <div className="flex items-center gap-4">
             <ThemeSwitcher />
             <UserButton  /> 
+            </div>
         </nav>
         <main className="flex w-full flex-grow">
         {children}
--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -2,6 +2,8 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { Computer, LaptopMinimal, Moon, Sun } from "lucide-react";
 
 const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme();
@@ -13,7 +15,19 @@ const ThemeSwitcher = () => {
 
     if (!mounted) return null;
     return (
-        <div>ThemeSwitcher</div>
+        <Tabs>
+            <TabsList className="border rounded-md">
+                <TabsTrigger value="light" className="cursor-pointer" onClick={()=>setTheme("light")}>
+                    <Sun className="size-5"/>
+                </TabsTrigger>
+                <TabsTrigger value="dark" className="cursor-pointer" onClick={()=>setTheme("dark")}>
+                    <Moon className="size-5"/>
+                </TabsTrigger>
+                <TabsTrigger value="system" className="cursor-pointer" onClick={()=>setTheme("system")}>
+                    <LaptopMinimal className="size-5"/>
+                </TabsTrigger>
+            </TabsList>
+        </Tabs>
     );
 }
 
